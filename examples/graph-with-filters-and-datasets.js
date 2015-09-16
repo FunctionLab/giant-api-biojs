d87c3c37fc0743507c84e08a4feb1914daf13f46
amr4 <amr3545@gmail.com>
2015-09-16 15:28:16 -0400

    Initial commit of examples

---
+++ examples/graph-with-filters-and-datasets.js
@@ -0,0 +1,2 @@
+var app = require("giant-api-biojs");
+app.graphWithFiltersAndDatasets({el: myDiv, tissue: 'Lung', genes: 'BRCA1,BRCA2'});
---
+++ examples/graph-with-filters.js
@@ -0,0 +1,2 @@
+var app = require("giant-api-biojs");
+app.graphWithFilters({el: myDiv, tissue: 'Lung', genes: 'BRCA1,BRCA2'});
